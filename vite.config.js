889754dfdbea061a2bdafbc67477d7bcf3cfb00a
hackwebdev <hackwebdev@gmail.com>
2022-07-30 16:44:38 +0800

    working vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
+// import path from 'path'
 import postcssNested from 'postcss-nested'
 import postcssSimpleVars from 'postcss-simple-vars'
 import postcssMixins from 'postcss-mixins'
 
 export default defineConfig({
+  root: './',
+  base: './',
+  assetsInclude: ['./assets/**/*.*'],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img'
+          }
+          if (/woff|woff2|ttf/i.test(extType)) {
+            extType = 'fonts'
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+    emptyOutDir: true,
+    outDir: './dist',
+  },
   css: {
     postcss: {
       plugins: [postcssNested, postcssSimpleVars, postcssMixins],
